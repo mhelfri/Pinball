9b7c680eda1f5e9fd3b487b83c606e54de347d73
mhelfri <155662290+mhelfri@users.noreply.github.com>
2024-03-03 18:31:28 -0500

    Update and rename module_template.h to pinball.h

--- modules/pinball/pinball.h
+++ modules/pinball/pinball.h
@@ -1,7 +1,7 @@
 //=====[#include guards - begin]===============================================
 
-#ifndef _MODULE_TEMPLATE_H_
-#define _MODULE_TEMPLATE_H_
+#ifndef _PINBALL_H_
+#define _PINBALL_H_
 
 //=====[Declaration of public defines]=========================================
 
@@ -11,4 +11,4 @@
 
 //=====[#include guards - end]=================================================
 
-#endif // _MODULE_TEMPLATE_H_
+#endif // _PINBALL_H_
