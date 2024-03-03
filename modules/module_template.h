96ca187066eb5f60323d88b99f9ef6049d974c61
mhelfri <155662290+mhelfri@users.noreply.github.com>
2024-03-03 18:21:32 -0500

    Add files via upload

---
+++ modules/module_template.h
@@ -0,0 +1,14 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _MODULE_TEMPLATE_H_
+#define _MODULE_TEMPLATE_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _MODULE_TEMPLATE_H_
