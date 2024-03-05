4c1b7870430a7a907d0441e47fd7988ba93e76df
Tremaine-Richardson <trey7836@gmail.com>
2024-03-05 17:11:14 +0000

    finished servo_motor driver

--- modules/servo_motor/servo_motor.cpp
+++ modules/servo_motor/servo_motor.cpp
@@ -7,9 +7,14 @@ v//=====[Libraries]=============================================================
 
 //=====[Declaration of private defines]========================================
 
+#define PERIOD          .02
+
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
+
+PwmOut right_flipper(PF_7);
+PwmOut left_flipper(PF_9);
 
 //=====[Declaration of external public global variables]=======================
 
@@ -21,4 +26,19 @@ v//=====[Libraries]=============================================================
 
 //=====[Implementations of public functions]===================================
 
+void servoInit()
+{
+    right_flipper.period(PERIOD);
+    left_flipper.period(PERIOD);
+}
+
+void rightFlipperWrite( float cycle )
+{
+    right_flipper.write(cycle);
+}
+
+void leftFlipperWrite( float cycle )
+{
+    left_flipper.write(cycle);
+}
 //=====[Implementations of private functions]==================================
--- modules/servo_motor/servo_motor.h
+++ modules/servo_motor/servo_motor.h
@@ -5,9 +5,21 @@
 
 //=====[Declaration of public defines]=========================================
 
+#define DUTY_RIGHT_MIN      .021
+#define DUTY_RIGHT_MAX      .1098
+#define DUTY_LEFT_MIN       .021
+#define DUTY_LEFT_MAX       .1079
+
+#define DUTY_RIGHT_66       .04
+#define DUTY_LEFT_66        .04
+
 //=====[Declaration of public data types]======================================
 
 //=====[Declarations (prototypes) of public functions]=========================
+
+void servoInit();
+void rightFlipperWrite( float cycle );
+void leftFlipperWrite( float cycle );
 
 //=====[#include guards - end]=================================================
 
